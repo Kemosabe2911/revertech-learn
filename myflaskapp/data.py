3633012cf1f64ad28832ce13309dae92e6f56037
Kemosabe2911 <stevinprince29@gmail.com>
2021-07-30 19:07:53 +0530

    add data.py

---
+++ myflaskapp/data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles= [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, dicta cum minus neque alias, incidunt commodi aspernatur quo iure, natus ab. Architecto harum, blanditiis assumenda commodi ex esse aspernatur perferendis?',
+            'author': 'John Doe',
+            'create_date': '30-07-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, dicta cum minus neque alias, incidunt commodi aspernatur quo iure, natus ab. Architecto harum, blanditiis assumenda commodi ex esse aspernatur perferendis?',
+            'author': 'Jane Doe',
+            'create_date': '30-07-2021'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, dicta cum minus neque alias, incidunt commodi aspernatur quo iure, natus ab. Architecto harum, blanditiis assumenda commodi ex esse aspernatur perferendis?',
+            'author': 'Jake Doe',
+            'create_date': '30-07-2021'
+        }
+    ]
+
+    return articles
