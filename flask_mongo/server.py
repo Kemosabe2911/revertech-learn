e8c7d05e99bcb8b1ec62351fd2af96e40fa03681
Kemosabe2911 <stevinprince29@gmail.com>
2021-08-05 20:00:43 +0530

    initialize server

--- flask_mongo/server.py
+++ flask_mongo/server.py
@@ -1,1 +1,13 @@
-print('x')
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route('/users', methods =['POST'])
+def create_user():
+    return "x"
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
